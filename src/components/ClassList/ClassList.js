1403a0151b128a11840735e938823dc2e4989c81
unknown <tyrome.douglass@gmail.com>
2019-10-22 14:53:22 -0600

    finished

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
+    super();
+
+    this.state = {
+      students: []
+    };
+  }
+
+  componentDidMount() {
+    axios 
+    .get(
+      `http://localhost:3055/students?class=${this.props.match.params.class}`
+    )
+    .then(result => {
+      this.setState({
+        students: results.data
+      });
+    });
   }
 
   render() {
+    const studnets = this.state.students.map((student, i) => (
+      <Link to={`/student/${student.id}`} key={i}>
+      <h3>
+        {student.first_name} {student.last_name}
+      </h3>
+      </Link>
+    ));
+
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {students}
       </div>
-    )
+    );
   }
 }
